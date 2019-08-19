1080cccc036d1c4c5b392bf8f011e99e4387d40c
muitsfriday <mu.itsfriday@gmail.com>
2019-08-19 23:56:29 +0700

    init 2

--- services/auth/main.go
+++ services/auth/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+	port := os.Getenv("PORT")
+
+	fmt.Printf("%s", port)
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":1323"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
